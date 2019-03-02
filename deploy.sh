5e64f54fde3d51abe844deb46ab2ce6d904c9096
stunstunstun <wjdsupj@gmail.com>
2019-03-02 20:50:05 +0900

    :pencil2: Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-touch out/.nojekyll && touch out/CNAME && echo \"stresscompany.co.kr\" >> out/CNAME && git add out/ && git commit -m \"Deploy to gh-pages\" 
-git subtree push --prefix out origin gh-pages"
+yarn buildy
+touch out/.nojekyll && touch out/CNAME && echo "stresscompany.co.kr" >> out/CNAME
+git add out/
+git commit -m "Deploy to gh-pages"
+git subtree push --prefix out origin gh-pages
